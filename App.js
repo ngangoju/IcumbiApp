2c4ca7067efeb524e0bd084b4d9a9519906e99aa
mutabazimat <mutabazimg@gmail.com>
2019-01-15 21:10:07 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,22 +1,16 @@
 import React from 'react';
+import Main from './App/components/main';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Welcome to Icumbi App</Text>
-        <Text>Coding is good</Text>
-      </View>
+      <Main />
+
+
+      
+      
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
